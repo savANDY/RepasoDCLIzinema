0eb5bde6a046b059ca2b49b6c7242459a2d9877d
savANDY <andicuu@gmail.com>
2017-10-26 14:18:47 +0200

    Funciona, todo correcto

--- js/zinema.js
+++ js/zinema.js
@@ -1,17 +1,24 @@
 var TotalButacas =[100,150,75,50];
 var ButacasLibres=[100,150,75,50];
 numSala=0;
+var numeroEntradas = 0;
 var nombre = "nada";
+var totalApagar = 0;
 
 function butacas(numeroSala) {
   document.getElementById("salacompra").innerHTML = ("Total butacas: " + TotalButacas[numeroSala] + " Butacas libres: " + ButacasLibres[numeroSala]);
 }
+
 
 function cambiarNombreBoton(nombre){
   document.getElementById("botonComprar").name = nombre;
 }
 
 function mostrarDetalles(sala) {
+  // Vaciar inputs
+  document.getElementById("numeroent").value = "";
+  document.getElementById("total").value = "";
+  // funcion:
   todasSalas = document.getElementsByClassName("texto");
   for (i = 0; i < todasSalas.length; i++){
     todasSalas[i].style.display = "none";
@@ -49,10 +56,35 @@ function mostrarDetalles(sala) {
 
 }
 
-function comprarEntradas(nombreBoton) {
-  switch (nombreBoton){
-    case "botonSala1":
-        ButacasLibres[0];
+function comprarEntradas() {
+  numeroEntradas = document.getElementById("numeroent").value;
+  var e = document.getElementById("tipo");
+  var tipoEntradas = e.options[e.selectedIndex].value;
 
+  totalApagar = numeroEntradas * tipoEntradas;
+
+  document.getElementById("precio").value = tipoEntradas;
+  document.getElementById("total").value = totalApagar;
+}
+
+function restarButacas(nombreBoton) {
+  numeroEntradas = document.getElementById("numeroent").value;
+  switch (nombreBoton){
+        case "botonSala1":
+        ButacasLibres[0] = (ButacasLibres[0] - numeroEntradas);
+        butacas(1 - 1);
+        break;
+    case "botonSala2":
+        ButacasLibres[1] = (ButacasLibres[1] - numeroEntradas);
+        butacas(2 - 1);
+        break;
+    case "botonSala3":
+        ButacasLibres[2] = (ButacasLibres[2] - numeroEntradas);
+        butacas(3 - 1);
+        break;
+    case "botonSala4":
+        ButacasLibres[3] = (ButacasLibres[3] - numeroEntradas);
+        butacas(4 - 1);
+        break;
   }
 }
